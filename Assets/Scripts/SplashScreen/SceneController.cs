69655efe5521664a9020570be9028ff06078beae
veggiemania <milkcowgames@gmail.com>
2025-09-26 17:03:32 +0200

    Añadida la escena SplashScreen

---
+++ Assets/Scripts/SplashScreen/SceneController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // Necesario para cambiar de escena
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Invoke("LoadNextScene", 3.0f);
+    }
+
+     void LoadNextScene()
+    {
+        SceneManager.LoadScene("Juego");
+    }
+}
